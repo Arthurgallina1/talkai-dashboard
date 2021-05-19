1b40b5f5965c4f3fa0d4856a5019113f891ed7e3
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-05-19 08:32:07 -0300

    add theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { ThemeProvider, Toolbar, AppBar, Typography } from '@material-ui/core/'
+import Orders from 'components/Orders'
+import { theme } from 'theme'
 
 function App() {
   return (
-    <div className="App">
-      <h1>teste!!</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <AppBar position="fixed">
+          <Toolbar variant="dense">
+            <Typography variant="h6">Multi-Step Form</Typography>
+          </Toolbar>
+        </AppBar>
+        <Orders />
+      </div>
+    </ThemeProvider>
   )
 }
 
